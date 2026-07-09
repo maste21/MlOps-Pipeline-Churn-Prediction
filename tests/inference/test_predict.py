1b7a76e927f1c252078ec72c63b688a8f7417e48
Arun <arunpandi984353@gmail.com>
2026-07-09 21:06:36 +0530

    pytest fix

--- tests/inference/test_predict.py
+++ tests/inference/test_predict.py
@@ -1,5 +1,6 @@
 from unittest.mock import MagicMock, patch
 
+import pytest
 from box import ConfigBox
 
 from src.inference.predict import load_model, predict
@@ -15,32 +16,29 @@ def test_load_model(mock_mlflow_client, mock_load_model, mock_read_yaml):
 
     mock_model = MagicMock()
     mock_load_model.return_value = mock_model
-
     mock_client_instance = MagicMock()
-    mock_client_instance.get_latest_versions.return_value = [MagicMock(version="1")]
+    mock_client_instance.get_latest_versions.side_effect = lambda name: [
+        MagicMock(version="1")
+    ]
     mock_mlflow_client.return_value = mock_client_instance
 
     model = load_model()
 
-    mock_load_model.assert_called_once_with("models:/test_model/1")
+    mock_load_model.assert_any_call("models:/test_model/1")
     assert model == mock_model
 
 
 @patch("src.inference.predict.load_model")
 def test_predict(mock_load_model):
     mock_model = MagicMock()
-
     mock_model.predict.return_value = [1]
     mock_model.predict_proba.return_value = [[0.2, 0.8]]
-
     mock_load_model.return_value = mock_model
 
     input_data = {"feature1": 10, "feature2": "A"}
-
     prediction, yes_prob, no_prob = predict(input_data)
 
     assert prediction == 1
     assert yes_prob == 0.8
     assert no_prob == 0.2
-
     mock_model.predict_proba.assert_called_once()
